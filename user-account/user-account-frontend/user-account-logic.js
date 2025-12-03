fdf618fe7de02819a907a6fff92889de56a278d7
padre_paulas <pounowyou@gmail.com>
2025-12-03 23:51:15 +0200

    Link user account page to db

--- user-account/user-account-frontend/user-account-logic.js
+++ user-account/user-account-frontend/user-account-logic.js
@@ -1,6 +1,7 @@
 import { auth } from '../../config/firebase-config.js';
 import { signOut } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 
 
 const signUpButton = document.getElementById("sign-up-button");
@@ -9,6 +10,8 @@ const password = document.getElementById("password-input");
 const signUpGoogleButton = document.getElementById("sign-up-with-google-button");
 const logOutButton = document.getElementById("sign-out-button");
 const emailElem = document.getElementById("user-email");
+const sessionsNumber = document.getElementById("number-sessions");
+const hoursFocused = document.getElementById("number-seconds");
   
 // if (signUpButton) {
 //   const signUp = async () => {
@@ -43,15 +46,45 @@ const emailElem = document.getElementById("user-email");
 //   logOutButton.onclick = logOut;
 // }
 
+
+
+const db = getFirestore();
+
+const getInfoFromDB = async () => {
+  const user = auth.currentUser;
+  if (!user) return;
+
+  const userRef = doc(db, "userStats", user.uid);
+  try {
+    const userSnap = await getDoc(userRef);
+    if (userSnap.exists()) {
+      const data = userSnap.data();
+      const timersFinished = data.timersFinished ?? 0;
+      const numberSeconds = data.secondsFocused ?? 0;
+      sessionsNumber.innerHTML = timersFinished.toString();
+      hoursFocused.innerHTML = (numberSeconds / 3600).toFixed(2).toString();
+    } else {
+      console.log("No doc found for the user");
+      sessionsNumber.innerHTML = "0";
+      hoursFocused.innerHTML = "0";
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+
 const showUserInfo = () => {
   emailElem.innerHTML = auth?.currentUser?.email;
-
 }
 showUserInfo();
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
     showUserInfo();
+    getInfoFromDB();
   }
 })
 
